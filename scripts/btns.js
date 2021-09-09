f919a118906ba6665225ba95fa35c44b1f264004
Raju Jai <rajujaijangra@gmail.com>
2021-09-09 07:28:00 +0530

    initial commit t-app v1

---
+++ scripts/btns.js
@@ -0,0 +1,22 @@
+function exchange(func){
+    let btn = document.getElementById("change");
+    let lan1 = document.getElementById("lan1");
+    let lan2 = document.getElementById("lan2");
+    btn.addEventListener("click", ()=>{
+        let temp1 = lan1.selectedIndex;
+        let temp2 = lan2.selectedIndex;
+        lan1.selectedIndex = temp2;
+        lan2.selectedIndex = temp1;
+        func();
+    })
+}
+
+function copy(){
+    let text = document.getElementById("outp");
+    text.select();
+    text.setSelectionRange(0, 99999);
+    navigator.clipboard.writeText(text.value);
+    console.log(text.value);
+}
+
+export {exchange, copy}
